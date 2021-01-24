dd28e95cfd913eecaacb654d5d918f67b7af9a2a
Hammer86gn <hammer86gn@aol.com>
2021-01-23 21:02:46 -0500

    Added GatewayIntents

--- src/main/java/me/hammer86gn/discordjar/DJAR.java
+++ src/main/java/me/hammer86gn/discordjar/DJAR.java
@@ -9,23 +9,27 @@
 
 package me.hammer86gn.discordjar;
 
+import com.google.gson.JsonParser;
 import me.hammer86gn.discordjar.connection.websocket.WebSocketClient;
 
 public class DJAR {
     public static DJAR instance;
     private String token;
+    public static JsonParser JSON_PARSER;
     //URL: wss://gateway.discord.gg/?v=8&encoding=json
 
     public DJAR() {
         instance = this;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DJAR djar = new DJAR();
-        djar.build("notokenforyou");
+        djar.build("ODAxMjgzODUzMjAwNjU0NDA2.YAebvQ.Yt2bWesPM3F3YQM7xGBNNr7KZnc");
 
         WebSocketClient client = new WebSocketClient("wss://gateway.discord.gg/?v=8&encoding=json",djar);
         client.connect();
+
+
     }
 
     public void build(String token) {
--- src/main/java/me/hammer86gn/discordjar/connection/websocket/WebSocketClient.java
+++ src/main/java/me/hammer86gn/discordjar/connection/websocket/WebSocketClient.java
@@ -12,16 +12,19 @@ package me.hammer86gn.discordjar.connection.websocket;
 
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import me.hammer86gn.discordjar.DJAR;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
 
+import static me.hammer86gn.discordjar.DJAR.JSON_PARSER;
+
 public class WebSocketClient extends org.java_websocket.client.WebSocketClient{
 
     private String url;
     private boolean isConnected = false;
-    private long heartbeatInterval = 45000;
+    private long heartbeatInterval;
     public DJAR djar;
 
     public WebSocketClient(String url, DJAR djar) {
@@ -39,24 +42,59 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient{
         JsonObject identifyD = new JsonObject();
         JsonObject identifyDProperties = new JsonObject();
 
+        /**
+         * The 'D' Value Properties Constructor for the Identification payload
+         */
         identifyDProperties.addProperty("$os","Windows");
         identifyDProperties.addProperty("$browser","Discord.jar");
         identifyDProperties.addProperty("$device","Discord.jar");
 
+        /**
+         * The 'D' Value Constructor for the Identification payload
+         */
         identifyD.addProperty("token",djar.getToken());
         identifyD.addProperty("intents",513);
         identifyD.add("properties",identifyDProperties);
 
-
+        /**
+         * The Constructor for the Identification payload
+         */
         identifyConnectionPacket.addProperty("op",2);
         identifyConnectionPacket.add("d",identifyD);
 
         send(identifyConnectionPacket.toString());
+        isConnected = true;
     }
 
     @Override
     public void onMessage(String s) {
         System.out.println(s);
+
+        JsonObject receivedData = JSON_PARSER.parse(s).getAsJsonObject();
+        String type = receivedData.get("t").toString();
+        JsonObject details = receivedData.get("d").getAsJsonObject();
+        if (type.equals("READY")) {
+            heartbeatInterval = receivedData.get("heartbeat_interval").getAsLong();
+            new Thread(() -> {
+                while (!getConnection().isClosed()) {
+                    JsonObject heartbeat = new JsonObject();
+                    heartbeat.addProperty("op",1);
+                    heartbeat.addProperty("d",System.currentTimeMillis());
+                    send(heartbeat.toString());
+                    try {
+                        Thread.sleep(heartbeatInterval);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        System.exit(0);
+                    }
+                }
+            }).start();
+        }
+        switch (type) {
+            case "READY":
+                System.out.println("Ready");
+                break;
+        }
     }
 
     @Override
@@ -67,5 +105,9 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient{
     @Override
     public void onError(Exception e) {
         e.printStackTrace();
+    }
+
+    public boolean isConnected() {
+        return isConnected;
     }
 }
