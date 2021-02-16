b427a531833b93a4dd8c935490a0bb004711e7f8
Hammer86gn <hammer86gn@aol.com>
2021-02-15 23:50:28 -0500

    Added VoiceChannel

--- src/main/java/me/hammer86gn/discordjar/discord/channel/guild/VoiceChannel.java
+++ src/main/java/me/hammer86gn/discordjar/discord/channel/guild/VoiceChannel.java
@@ -6,69 +6,61 @@ import me.hammer86gn.discordjar.discord.channel.IChannel;
 import java.security.InvalidParameterException;
 import java.util.Arrays;
 
-public class NewsChannel implements IChannel {
+public class VoiceChannel implements IChannel {
 
     private final String id;
     private final String guild_id;
     private final String name;
     private final int type;
+    private final boolean nsfw;
     private final int position;
     private final String[] permission_overwrites;
-    private final boolean nsfw;
-    private final String topic;
-    private final String last_message_id;
+    private final int bitrate;
+    private final int user_limit;
     private final String parent_id;
     private final JsonObject channelObject;
 
-    public NewsChannel(String id, String guild_id, String name, int position, String permission_overwrites[], boolean nsfw, String topic, String last_message_id, String parent_id) {
+    public VoiceChannel(String id, String guild_id, String name, boolean nsfw, int position, String[] permission_overwrites, int bitrate, int user_limit, String parent_id) {
         this.id = id;
         this.guild_id = guild_id;
         this.name = name;
-        this.type = 5;
+        this.type = 2;
+        this.nsfw = nsfw;
         this.position = position;
         this.permission_overwrites = permission_overwrites;
-        this.nsfw = nsfw;
-        this.topic = topic;
-        this.last_message_id = last_message_id;
+        this.bitrate = bitrate;
+        this.user_limit = user_limit;
         this.parent_id = parent_id;
         this.channelObject = new JsonObject();
 
-        String discordFriendlyName = name.toLowerCase().replace(' ','-');
-
-        if (discordFriendlyName.toCharArray().length > 100) {
-            throw new InvalidParameterException("GuildNewsChannel names can only be a maximum of 100 characters");
-        }
-        if (discordFriendlyName.toCharArray().length < 2) {
-            throw new InvalidParameterException("GuildNewsChannel names can only be a minimum of 2 characters");
-        }
-        if (topic.toCharArray().length > 1024) {
-            throw new InvalidParameterException("GuildNewsChannel topics can only be a maximum of 1024 characters");
+        if (name.toCharArray().length > 100) {
+            throw new InvalidParameterException("GuildVoiceChannel names can only be a maximum of 100 characters");
         }
 
         channelObject.addProperty("id",id);
         channelObject.addProperty("guild_id",guild_id);
-        channelObject.addProperty("name",name);
+        channelObject.addProperty("name", name);
         channelObject.addProperty("type",type);
+        channelObject.addProperty("nsfw",nsfw);
         channelObject.addProperty("position",position);
         channelObject.addProperty("permission_overwrites", Arrays.toString(permission_overwrites));
-        channelObject.addProperty("nsfw",nsfw);
-        channelObject.addProperty("topic",topic);
-        channelObject.addProperty("last_message_id",last_message_id);
-        channelObject.addProperty("parent_id", parent_id);
+        channelObject.addProperty("bitrate",bitrate);
+        channelObject.addProperty("user_limit",user_limit);
+        channelObject.addProperty("parent_id",parent_id);
     }
 
     /*
 {
-  "id": "41771983423143937",
+  "id": "155101607195836416",
   "guild_id": "41771983423143937",
-  "name": "important-news",
-  "type": 5,
-  "position": 6,
+  "name": "ROCKET CHEESE",
+  "type": 2,
+  "nsfw": false,
+  "position": 5,
   "permission_overwrites": [],
-  "nsfw": true,
-  "topic": "Rumors about Half Life 3",
-  "last_message_id": "155117677105512449",
-  "parent_id": "399942396007890945"
+  "bitrate": 64000,
+  "user_limit": 0,
+  "parent_id": null
 }
      */
 
@@ -117,12 +109,11 @@ public class NewsChannel implements IChannel {
         return parent_id;
     }
 
-    public String getLastMessageID() {
-        return last_message_id;
+    public int getBitrate() {
+        return bitrate;
     }
 
-    public String getTopic() {
-        return topic;
+    public int getUserLimit() {
+        return user_limit;
     }
-
 }
